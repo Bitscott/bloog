762290ff54c585cc7aab353e4fd3cb0a1f3158c4
Bitscott <cursos@gpourma.com>
2014-01-26 10:14:06 -0800

    heroku chenges

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+
+  	@articles = Article.all
+
+  end
+
+  def new
+  	
+  end
+
+  def edit
+  end
+
+  def show
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloog::Application.config.secret_token = 'c0bf14624f34a3075521448be4294b1b7282aeceebacea4f820e683558e3aa51ee5665789706ea3ffc57d32217190e1e35a19b6988eaf43c97d08e9b07cf85a0'
