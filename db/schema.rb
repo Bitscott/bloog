319d790fd6c7db9cbf9fd3e7dafead67e470c185
Scott1000 <scott.agua@gmail.com>
2014-01-27 13:36:56 -0800

    create the comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140125191231) do
+ActiveRecord::Schema.define(:version => 20140127203600) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "comments", :force => true do |t|
+    t.string   "content"
+    t.integer  "article_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
